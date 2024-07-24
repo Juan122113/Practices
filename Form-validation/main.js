e5bdd3f3ade58df8553ab4e689cd43450b89dffc
Juan122113 <ammiel89@hotmail.com>
2024-07-24 16:40:46 -0300

    Updates

--- Form-validation/main.js
+++ Form-validation/main.js
@@ -1,45 +1,9 @@
-// const screen = document.getElementById('screen');
-// const redBtn = document.getElementById('red');
-// const greenBtn = document.getElementById('green');
-// const blueBtn = document.getElementById('blue');
-
-
-// redBtn.addEventListener('click', (e) => {
-//     screen.style.backgroundColor = 'red';
-// });
-
-// greenBtn.addEventListener('click', (e) => {
-//     screen.style.backgroundColor = 'green';
-// });
-
-// blueBtn.addEventListener('click', () => {
-//     screen.style.backgroundColor = 'blue';
-// });
-
-// const buttons = document.querySelectorAll('button');
-
-// whit a forEach loop
-
-// buttons.forEach((i) => {
-//     i.addEventListener('click', (e) => {
-//         screen.style.backgroundColor = e.target.id;
-//     });
-// });
-
 const form = document.getElementById('form');
 
 function handleSubmit(e) {
     e.preventDefault() // prevent the default behaviour
 
-    // const formData = new FormData(e.target);
-    // const data = Object.fromEntries(formData);
-    // const data = {};
-    // const fields = e.target.querySelectorAll("input, select, textarea");
     const data = Object.fromEntries(new FormData(e.target));
-
-    // for (const field of fields) {
-    //     data[field.name] = field.value;
-    // }
 
     for (const dat in data) {
         if (data[dat].length > 0) {
@@ -79,9 +43,6 @@ function handleSubmit(e) {
     }
 
     console.log(data);
-    //console.log(formData);
-    // console.log(fields);
 }
 
 form.addEventListener('submit', handleSubmit);
-
